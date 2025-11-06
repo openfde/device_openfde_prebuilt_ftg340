3884d076ad4db5dfa7f48bd26d3abed1263417b4
tb <ali609386314@gmail.com>
2025-11-06 15:00:11 +0800

    for gpu-driver_1.1.16.1103test
    rename to ftg340

--- prebuilts.mk
+++ prebuilts.mk
@@ -1,18 +1,17 @@
 ifneq ($(TARGET_HAVE_LEOPARD),false)
 LEOPARD_PRODUCT_PACKAGES := \
-    gralloc.LEOPARD \
-    libdrm_leopard \
-    libEGL_LEOPARD \
-    libGLESv1_CM_LEOPARD \
-    libGLESv2_LEOPARD \
+    gralloc.FTG340 \
+    libdrm_ftg340 \
+    libEGL_FTG340 \
+    libGLESv1_CM_FTG340 \
+    libGLESv2_FTG340 \
     libGAL \
     libGLSLC \
     libVSC \
-    libvulkan_LEOPARD \
-    vulkan.LEOPARD \
-    libSPIRV_LEOPARD \
+    libvulkan_FTG340 \
+    vulkan.FTG340 \
+    libSPIRV_ftg \
     libdrm_android
-
 PRODUCT_PACKAGES += \
         $(LEOPARD_PRODUCT_PACKAGES)
 endif
